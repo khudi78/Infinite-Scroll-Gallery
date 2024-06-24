99aac57c1c822c4684db0acdab5bf5da5c0e9c4b
Khushi <krkhushi2001@gmail.com>
2024-06-24 19:42:56 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-    <header className=" bg-emerald-900 rounded-3xl text-white p-4 text-center">
+    <header className=" bg-emerald-900 rounded-3xl text-white p-4 text-center max-[640px]:w-[400px] max-[640px]:ml-7">
       <h1 className="text-2xl font-bold">Infinite Scroll Gallery</h1>
     </header>
     <main>
--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -43,7 +43,7 @@ const Gallery = () => {
 
   return (
     <div className="container mx-auto px-4 py-8 w-[1300px]">
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-16 w-[1250px]">
+      <div className="grid grid-cols-2 lg:ml-[-10px] sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 lg:gap-y-16 md:gap-y-16 sm:gap-y-16 lg:w-[1200px] md:w-[700px] sm:gap-x-16 sm:ml-[50px] sm:w-[490px] max-[640px]:gap-y-16 max-[640px]:ml-[100px]">
         {images.map((image, index) => (
           <Lazyload
             key={index}
